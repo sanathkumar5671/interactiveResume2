2b45e462a2994a5fac827546ebbdb2f3c145e45e
sanath kumar <sanathkumar5671@gmail.com>
2024-05-30 23:39:38 +1000

    making personal changes to llamaIndex create

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,28 +1,11 @@
-import Image from "next/image";
-
 export default function Header() {
+
   return (
-    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-      <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-        Get started by editing&nbsp;
-        <code className="font-mono font-bold">app/page.tsx</code>
-      </p>
-      <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-        <a
-          href="https://www.llamaindex.ai/"
-          className="flex items-center justify-center font-nunito text-lg font-bold gap-2"
-        >
-          <span>Built by LlamaIndex</span>
-          <Image
-            className="rounded-xl"
-            src="/llama.png"
-            alt="Llama Logo"
-            width={40}
-            height={40}
-            priority
-          />
-        </a>
+      <div className="flex flex-col items-center">
+
+<h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Interactive Résumé</span> By Sanath.</h1>
+<p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Hiring made easy for recuriters, please ask any questions about Sanath's Career Journey.</p>
+
       </div>
-    </div>
   );
 }
